3da827737ef029d7e0c385cef4f39fc541309da9
Octavian Regatun <octavian.regatun@outlook.com>
2020-09-13 15:55:52 +0300

    add site

---
+++ precache-manifest.0d0914b058b4d81d8c4f1c804bd2cb96.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a684944fd41ae16b9d16aa438cdd32e3",
+    "url": "/octavian-regatun.github.io/index.html"
+  },
+  {
+    "revision": "75b8ba21e6508e4236be",
+    "url": "/octavian-regatun.github.io/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "824916d07341b3b460b6",
+    "url": "/octavian-regatun.github.io/static/js/2.29138628.chunk.js"
+  },
+  {
+    "revision": "75b8ba21e6508e4236be",
+    "url": "/octavian-regatun.github.io/static/js/main.025246d5.chunk.js"
+  },
+  {
+    "revision": "df102450be2df52552d7",
+    "url": "/octavian-regatun.github.io/static/js/runtime-main.b6a13062.js"
+  },
+  {
+    "revision": "e850480d996e04519e79f9db0b76fb4c",
+    "url": "/octavian-regatun.github.io/static/media/image1.e850480d.svg"
+  },
+  {
+    "revision": "f6cb90e7090bdb50d9f2ec4d273e2dfa",
+    "url": "/octavian-regatun.github.io/static/media/logo.f6cb90e7.png"
+  },
+  {
+    "revision": "96153f08978588ed390fa5c356f0ef8d",
+    "url": "/octavian-regatun.github.io/static/media/profilepic.96153f08.png"
+  },
+  {
+    "revision": "948483a59b4f56937328518c40854c06",
+    "url": "/octavian-regatun.github.io/static/media/unity.948483a5.png"
+  }
+]);
